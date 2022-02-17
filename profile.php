5d0541725c26900cdf33ba8287dfcaa276fdc81a
kaileyhaynes <84878360+kaileyhaynes@users.noreply.github.com>
2022-02-16 22:29:25 -0500

    testing out dynamic pages

--- profile.php
+++ profile.php
@@ -30,42 +30,21 @@ $stmt->close();
 
 ?>
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Profile</title>
-		<link href="dashboardStyle.css" rel="stylesheet" type="text/css">
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-	</head>
-	<body class="loggedin">
-		<nav class="navtop">
-			<div>
-				<h1>Ticketerr</h1>
-				<a href = "dashboardHome.php"><i class = "fas fa-home"></i>Home</a>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-			</div>
-		</nav>
-		<div class="content">
-			<h2>Profile</h2>
-			<div>
-				<p>Your account details are below:</p>
-				<table>
-					<tr>
-						<td>Username:</td>
-						<td><?=$_SESSION['name']?></td>
-					</tr>
-					<tr>
-						<td>Password:</td>
-						<td><?=$password?></td>
-					</tr>
-					<tr>
-						<td>Email:</td>
-						<td><?=$email?></td>
-					</tr>
-				</table>
-			</div>
-		</div>
-	</body>
-</html>
+<?php echo file_get_contents("navtop.html"); ?>
+<?php echo file_get_contents("navleft.html"); ?>
+	<h2>Profile</h2>
+	<p>Your account details are below:</p>
+		<table>
+			<tr>
+				<td>Username:</td>
+				<td><?=$_SESSION['name']?></td>
+			</tr>
+			<tr>
+					<td>Password:</td>
+					<td><?=$password?></td>
+				</tr>
+				<tr>
+					<td>Email:</td>
+					<td><?=$email?></td>
+				</tr>
+			</table>
